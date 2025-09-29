062d7a5677a103c7c1ba0386dc576617455684ad
EST2374 <jack.cez2004@gmail.com>
2025-09-29 11:59:04 +0200

    New_Features

--- client.py
+++ client.py
@@ -1,11 +1,76 @@
 import socket
 import threading
+import os
+import sys
+import argparse
 
-nickname = input("Choose a nickname: ")
+def parser():
 
+    Default_port = 9001
+
+    parser = argparse.ArgumentParser(
+        description="Chat Client. Needs IP and Port"
+    )
+
+    parser.add_argument(
+        '-i', '--ip',
+        type=str,
+        required=None,
+        help="IP of the Server (127.0.0.1)" 
+    )
+
+    parser.add_argument(
+        '-p', '--port',
+        type=int,
+        default=Default_port,
+        help=f"Port of the Server. Defualt Port is {Default_port}"
+    )
+
+    parser.add_argument(
+        '-u', '--user',
+        type=str,
+        default=None,
+        help="Your Username/Nickname"
+    )
+    
+    args = parser.parse_args()
+
+    if args.ip is None:
+        args.ip = input("Server IP: ")
+    if args.port == Default_port and '-p' not in sys.argv and '--port' not in sys.argv:
+        change_port = input(f"Use default Port {Default_port}? [Y/n]")
+        if change_port.lower() == 'n':
+          while True:
+              try: 
+                    new_port_str = input("Server Port: ")
+                    args.port = int(new_port_str)
+                    break
+              except ValueError:
+                    print("Invalid Port number. Please enter a number")
+          
+    if args.user is None:
+        args.user = input("Choose a nickname: ")
+    
+    return args
+
+if __name__ == "__main__":
+    args = parser()
+
+    IP = args.ip
+    Port = args.port
+    nickname = args.user
+
+    if not IP:
+        print("Error. Server IP cannot be empty. Exiting")
+        sys.exit(1)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 9001))
+try:
+    client.connect((IP, int(Port)))
+    print(f"Connected to {IP}:{Port}...")
+except ConnectionRefusedError:
+    print("Couldn't connect to the Server")
+    sys.exit(1)
 
 def receive():
     while True:
@@ -18,12 +83,24 @@ def receive():
         except:
             print("An error occured!")
             client.close()
+            os._exit(0)
             break
 
 def write():
     while True:
-        msg = f'{nickname}: {input("")}'
-        client.send(msg.encode('ascii'))
+        user_input = input("")
+        if user_input.lower() == '.exit':
+            print("Disconneting...")
+            client.close()
+            os._exit(0)
+            break
+
+        msg = f'{nickname}: {user_input}'
+        try:
+            client.send(msg.encode('ascii'))
+        except socket.error as e:
+            print(f"Error sending message: {e}")
+            break
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
